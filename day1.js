45fa5bfe035af1a5c6da50988778f8f374f38edb
Abel808 <able.kodes@gmail.com>
2023-12-21 21:22:48 +0530

    added js

---
+++ day1.js
@@ -0,0 +1,29 @@
+let name = 'able';
+
+let a = 2;
+let b =3;
+
+let sum = null;
+console.log(sum);
+
+name = 'able cajetan';
+let num = 34343;
+let bool = true;
+let naaal = null;
+let defined;
+let symbaal = Symbol ('ayeee');
+
+console.log(typeof num, typeof bool, typeof naaal, typeof defined, typeof symbaal);
+
+let obj = {
+    name:'able',
+    age: 32,
+    stack: ['html', 'css', bool],
+};
+
+console.log(obj);
+
+
+obj.name = 'able cajetan';
+
+console.log(obj.name);
